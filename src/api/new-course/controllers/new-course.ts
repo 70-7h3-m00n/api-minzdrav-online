eecb452e0f4cc24df3d667cd2a825def339f0ed8
AsgurDjons <asgyrdjons@gmail.com>
2024-04-16 12:40:36 +0300

    feat(new-course): Создание новой api для нового дизайна курсов

---
+++ src/api/new-course/controllers/new-course.ts
@@ -0,0 +1,7 @@
+/**
+ * new-course controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::new-course.new-course');
---
+++ src/api/new-course/services/new-course.ts
@@ -0,0 +1,7 @@
+/**
+ * new-course service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::new-course.new-course');
